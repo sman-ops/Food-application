99ab7974e1622989d6d59c945cb1724f638367ab
sman-ops <slimen.ghenimi@gmail.com>
2022-12-01 19:19:24 +0100

    add login page and api in backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import { sample_foods, sample_tags } from './data';
-
+import { sample_foods, sample_tags, sample_users } from './data';
+import jwt from 'jsonwebtoken';
 // Now this app is an express application
 const app = express();
+app.use(express.json());
 app.use(
   cors({
     credentials: true,
@@ -38,6 +39,34 @@ app.get('/api/foods/:foodId', (req, res) => {
   const food = sample_foods.find((food) => food.id === foodId);
   res.send(food);
 });
+
+app.post('/api/users/login', (req, res) => {
+  // email  inside the body will be  put in this variable : Destructuring assignment
+  const { email, password } = req.body;
+  const user = sample_users.find(
+    (user) => user.email === email && user.password === password
+  );
+  if (user) {
+    res.send(generateTokenResponse(user));
+  } else {
+    res.status(400).send('user or password is not valid !');
+  }
+});
+
+const generateTokenResponse = (user: any) => {
+  const token = jwt.sign(
+    {
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    'SomeRandomText',
+    {
+      expiresIn: '30d',
+    }
+  );
+  user.token = token;
+  return user;
+};
 
 const port = 4000;
 app.listen(port, () => {
