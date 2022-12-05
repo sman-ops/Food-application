7bcd2ee83dd30062bf7c80d6c3419e9f2c15ee19
sman-ops <slimen.ghenimi@gmail.com>
2022-12-05 15:35:27 +0100

    connecting to mongo and create routers folder

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import express from 'express';
 import cors from 'cors';
-import { sample_foods, sample_tags, sample_users } from './data';
-import jwt from 'jsonwebtoken';
+import foodRouter from './routers/food.router';
+import userRouter from './routers/user.router';
 // Now this app is an express application
 const app = express();
+dotenv.config();
 app.use(express.json());
 app.use(
   cors({
@@ -12,63 +15,28 @@ app.use(
   })
 );
 
-app.get('/api/foods', (req, res) => {
-  res.send(sample_foods);
-});
-
-app.get('/api/foods/search/:searchTerm', (req, res) => {
-  const { searchTerm } = req.params;
-  const foods = sample_foods.filter((food) =>
-    food.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
-  );
-  res.send(foods);
-});
-
-app.get('/api/foods/tags', (req, res) => {
-  res.send(sample_tags);
-});
-
-app.get('/api/foods/tag/:tagName', (req, res) => {
-  const { tagName } = req.params;
-  const foods = sample_foods.filter((food) => food.tags?.includes(tagName));
-  res.send(foods);
-});
-
-app.get('/api/foods/:foodId', (req, res) => {
-  const { foodId } = req.params;
-  const food = sample_foods.find((food) => food.id === foodId);
-  res.send(food);
-});
-
-app.post('/api/users/login', (req, res) => {
-  // email  inside the body will be  put in this variable : Destructuring assignment
-  const { email, password } = req.body;
-  const user = sample_users.find(
-    (user) => user.email === email && user.password === password
-  );
-  if (user) {
-    res.send(generateTokenResponse(user));
-  } else {
-    res.status(400).send('user or password is not valid !');
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL!);
+    console.log('Connected  to mongodb');
+  } catch (error) {
+    throw error;
   }
+};
+mongoose.connection.on('disconnected', () => {
+  console.log('mongodb disconnected');
 });
 
-const generateTokenResponse = (user: any) => {
-  const token = jwt.sign(
-    {
-      email: user.email,
-      isAdmin: user.isAdmin,
-    },
-    'SomeRandomText',
-    {
-      expiresIn: '30d',
-    }
-  );
-  user.token = token;
-  return user;
-};
+mongoose.connection.on('connected', () => {
+  console.log('mongodb connected');
+});
+
+// use this router  on this address
+app.use('/api/foods', foodRouter);
+app.use('/api/users', userRouter);
 
 const port = 4000;
 app.listen(port, () => {
+  connect();
   console.log('website served on  http://localhost:' + port);
 });
