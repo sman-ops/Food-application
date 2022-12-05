14255963a5c247d079f234f81ef34edda9af3fbe
sman-ops <slimen.ghenimi@gmail.com>
2022-12-05 21:45:26 +0100

    create routers and models folders

--- backend/src/routers/food.router.ts
+++ backend/src/routers/food.router.ts
@@ -1,11 +1,30 @@
 import { Router } from 'express';
 import { sample_foods, sample_tags } from '../data';
-
+import asyncHandler from 'express-async-handler';
+import { FoodModel } from '../models/food.model';
+// asyncHandler permet remplacer try and catch : no need to write try and catch in router handler
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send(sample_foods);
-});
+router.get(
+  '/seed',
+  asyncHandler(async (req, res) => {
+    const foodsCount = await FoodModel.countDocuments();
+    if (foodsCount > 0) {
+      res.send('Seed is already done!');
+      return;
+    }
+
+    await FoodModel.create(sample_foods);
+    res.send('Seed Is Done!');
+  })
+);
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const foods = await FoodModel.find();
+    res.send(foods);
+  })
+);
 
 router.get('/search/:searchTerm', (req, res) => {
   const { searchTerm } = req.params;
--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,7 +1,24 @@
 import { Router } from 'express';
 import { sample_users } from '../data';
 import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import { UserModel } from '../models/user.model';
 const router = Router();
+
+router.get(
+  '/seed',
+  asyncHandler(async (req, res) => {
+    const usersCount = await UserModel.countDocuments();
+    if (usersCount > 0) {
+      res.send('Seed is already done!');
+      return;
+    }
+
+    await UserModel.create(sample_users);
+    res.send('Seed Is Done!');
+  })
+);
+
 router.post('/login', (req, res) => {
   // email  inside the body will be  put in this variable : Destructuring assignment
   const { email, password } = req.body;
